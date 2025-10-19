4bc7e9d690afad78943f9f9ad67a6c8a854334c6
MarkHiarley <hiarleycostamarley@gmail.com>
2025-10-19 16:28:06 -0300

    Update database connection settings to use DB_PORT environment variable instead of DB_HOST_PORT

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -17,7 +17,7 @@ func ConnectDB() (*sql.DB, error) {
 	}
 
 	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_HOST_PORT")
+	port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
